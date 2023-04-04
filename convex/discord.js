04b6ffcc786c2c32959ab7de4e1bd950a8fbc4b8
Ian Macartney <ian@convex.dev>
2023-04-03 17:41:29 -0700

    get rid of discord interactions for now

--- convex/discord.js
+++ convex/discord.js
@@ -4,47 +4,24 @@ import {
   mutation,
   query,
 } from "./_generated/server";
-import {
-  InteractionType,
-  InteractionResponseType,
-  verifyKey,
-} from "discord-interactions";
 
-export const addDiscordMessage = internalMutation(({ db }, msg) => {
-  db.insert("discord", msg);
-});
-
-export const discordHandler = httpEndpoint(async ({ runMutation }, request) => {
-  const bodyText = await request.text();
-
-  // Check signature -- uses discord-interactions package
-  const isValidSignature = verifyKey(
-    bodyText,
-    request.headers.get("X-Signature-Ed25519"),
-    request.headers.get("X-Signature-Timestamp"),
-    process.env.DISCORD_PUBLIC_KEY
-  );
-  if (!isValidSignature) {
-    return new Response("invalid request signature", { status: 401 });
+const upsert = (db, table, doc, primaryKey = "id") => {
+  const existing = db
+    .query(table)
+    .filter((q) => q.eq(q.field(primaryKey), doc[primaryKey]))
+    .unique();
+  if (!existing) {
+    return db.insert(table, doc);
+  } else {
+    return db.replace(existing, doc);
   }
+};
 
-  const body = JSON.parse(bodyText);
-  console.log(body);
-  // Handle ping
-  if (body.type === InteractionType.PING) {
-    return new Response(
-      JSON.stringify({ type: InteractionResponseType.PONG }),
-      { status: 200 }
-    );
+export const addDiscordMessage = mutation(
+  ({ db }, { message, author, member, channels }) => {
+    upsert(db, "discordMessage", message);
+    upsert(db, "discordUser", author);
+    upsert(db, "discordMember", author);
+    db.insert("discordMessage", message);
   }
-
-  // If it's a message, record it.
-  // If it's from a user we don't know, look them up (& store them).
-  // From scheduled action?
-  // Look up the associated slack thread.
-  // Send it to slack.
-
-  return new Response(null, {
-    status: 200,
-  });
-});
+);
