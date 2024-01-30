af8af2aebf610930d257c15da8194038ba62e800
Ian Macartney <ianhmacartney@gmail.com>
2024-01-30 12:26:37 -0800

    just warn on missing algolia

--- convex/algolia.ts
+++ convex/algolia.ts
@@ -5,15 +5,15 @@ import algoliasearch from "algoliasearch";
 import { internalAction } from "./_generated/server";
 
 export const ALGOLIA_APP_ID = "1KIE511890";
-export function getAlgolia() {
-  const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY;
-  if (!ALGOLIA_API_KEY) throw new Error("ALGOLIA_API_KEY not set");
-  return algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
-}
 const DISCORD_INDEX = "discord";
 
 export const index = internalAction(async ({ runQuery, runMutation }) => {
-  const algolia = getAlgolia();
+  const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY;
+  if (!ALGOLIA_API_KEY) {
+    console.warn("ALGOLIA_API_KEY not set");
+    return;
+  }
+  const algolia = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
   const index = algolia.initIndex(DISCORD_INDEX);
   while (true) {
     const { documents, position } = await runQuery(
