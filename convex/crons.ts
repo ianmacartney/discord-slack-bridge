3604787f4c176df1a9470efe41bf2a4e665fd309
Ian Macartney <ianhmacartney@gmail.com>
2024-01-30 11:11:34 -0800

    non optional cron

--- convex/crons.ts
+++ convex/crons.ts
@@ -2,11 +2,9 @@ import { cronJobs } from "convex/server";
 import { internal } from "./_generated/api";
 
 const crons = cronJobs();
-if (process.env.ALGOLIA_API_KEY) {
-  crons.interval(
-    "Sync discord support threads with Algolia",
-    { minutes: 1 }, // Every minute
-    internal.algolia.index
-  );
-}
+crons.interval(
+  "Sync discord support threads with Algolia",
+  { minutes: 1 }, // Every minute
+  internal.algolia.index
+);
 export default crons;
