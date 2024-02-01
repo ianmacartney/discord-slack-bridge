a98ac4bdceb9e8fa86ded77d2680aabaf8ed462c
Ian Macartney <ianhmacartney@gmail.com>
2024-02-01 14:53:56 -0800

    make a monorepo

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import ConvexClientProvider from './ConvexClientProvider';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Notes App',
+  description: 'This is an app to take notes.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </body>
+    </html>
+  );
+}
